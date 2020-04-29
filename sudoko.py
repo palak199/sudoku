6022e84939d8538c4443e92d5afd946fdd798cc9
palak <p.thapar99@gmail.com>
2020-04-29 16:47:28 +0530

    initial commit

---
+++ sudoko.py
@@ -0,0 +1,96 @@
+
+def show_matrix(matrix):
+    for i in matrix:
+        print(' '.join(str(i)))
+        
+def make_matrix(n):
+    matrix=[]
+    for i in range(n):
+        temp=[]
+        for j in range(n):
+            temp.append(int(input()))
+        matrix.append(temp)
+    return matrix
+    
+nums=[1,2,3,4]
+    
+def column(matrix, i):
+    return [row[i] for row in matrix]  
+
+def sub_matrix(matrix,i,j):
+    sm=[]
+    i=i-1 if i%2==1 else i
+    j=j-1 if j%2==1 else j
+    for row in range(2):
+        temp=[]
+        for col in range(2):
+            temp.append(matrix[row+i][col+j])
+        sm.append(temp)
+    return (sm)
+
+def check_sub_matrix(sm,num):
+    if not any(num in x for x in sm):
+        return True
+    else:
+        return False
+    
+def check_row_matrix(row_matrix,num):
+    if num not in row_matrix:
+        return True
+    else:
+        return False
+    
+
+def check_col_matrix(col_matrix,num):
+    if num not in col_matrix:
+        return True
+    else:
+        return False
+    
+            
+def find_zero(matrix):
+    for i in range(4):
+        for j in range(4):
+            if matrix[i][j]==0:
+                return (i,j)
+            
+    return None
+                   
+    
+def solver(matrix):
+        
+    is_there=find_zero(matrix)
+    if (not is_there):        
+        return True
+    else:    
+        i,j=is_there
+    
+    
+    for t in nums:
+        sm=sub_matrix(matrix,i,j)
+        row_matrix=matrix[i]
+        col_matrix=column(matrix,j)                    
+        if check_row_matrix(row_matrix,t) and check_col_matrix(col_matrix,t) and check_sub_matrix(sm,t):
+                matrix[i][j]=t
+                if(solver(matrix)):      
+                    return True
+                matrix[i][j]=0
+    
+    return False
+                    
+if __name__=="__main__": 
+    test_matrix=[[0,0,4,3],[0,0,0,0],[0,0,0,0],[2,3,0,0]]  
+    if solver(test_matrix):
+        show_matrix(test_matrix)
+#show_matrix(make_matrix(2))
+
+
+        
+    
+   
+
+#print(sub_matrix(test_matrix,3,3))   
+#show_matrix(test_matrix)
+
+
+
