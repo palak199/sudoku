afecf755df57ec8df126f52973be3ef55d2cfa1b
palak <p.thapar99@gmail.com>
2020-05-03 21:57:27 +0530

    "GUI ersion of sudoku solver

--- gui.py
+++ gui.py
@@ -1,24 +1,46 @@
 import tkinter as tk
 from tkinter import Entry
-lst=[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+
 root=tk.Tk()
 root.title("sudoku solver")
 canvas=tk.Canvas(root,height=100,width=100)
 canvas.pack()
 height = 4
 width = 4
-b=[]
-for i in range(height): #Rows
-    for j in range(width): #Columns
-        b= Entry(canvas, width=10)
-        b.grid(row=i, column=j)
-        b.insert(0, lst[i][j])
-        
+b=matrix=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
+def result():
+    print("i")
+
+'''for i in range(height): #Rows
+    for j in range(width): #Columns
+        b[i][j]= Entry(canvas, width=10)
+        b[i][j].grid(row=i, column=j)
+        b[i][j].insert(0, lst[i][j])
+    '''
+
+def callback():
+    for i in range(4):
+        for j in range(4):
+            t=b[i][j].get()
+            if t!='':
+                matrix[i][j]=int(t)
+            else:
+                matrix[i][j]=0
+ 
+               
+
+def make_matrix():
+    for i in range(4):
+        for j in range(4):
+            b[i][j]= Entry(canvas, width=10)
+            b[i][j].grid(row=i, column=j)
+            #print(b[i][j].get())
     
     
-#button = tk.Button(root, text="get", width=10, command=callback)
-#button.pack()
+make_matrix()    
+button = tk.Button(root, text="get", width=10, command=callback)
+button.pack()
 
 root.mainloop()
 
--- sudoko.py
+++ sudoko.py
@@ -1,51 +1,86 @@
 import tkinter as tk
 from tkinter import Entry
-'''
-for cli version
-def show_matrix(matrix):
-    for i in matrix:
-        print(' '.join(str(i)))
-        '''
-
+from tkinter import messagebox
 #for GUI version
-def show_matrix(matrix):
-    root=tk.Tk()
-    root.title("sudoku solver")
-    canvas=tk.Canvas(root,height=100,width=100)
-    canvas.pack()
-    
-    for i in range(4): #Rows
-        for j in range(4): #Columns
+def result_matrix(res_matrix):
+    canvas2=tk.Canvas(root,height=100,width=100)
+    canvas2.pack()
+    for i in range(9): #Rows
+        for j in range(9): #Columns
             b= Entry(canvas, width=10)
             b.grid(row=i, column=j)
-            b.insert(0, matrix[i][j])
-    root.mainloop()
+            b.insert(0, res_matrix[i][j])
+
         
 
 
         
-def make_matrix(n):
-    matrix=[]
-    for i in range(n):
-        temp=[]
-        for j in range(n):
-            print ("add->",i,j,":",end="")
-            temp.append(int(input()))
-        matrix.append(temp)
-    return matrix
+# def make_matrix(n):
+#     matrix=[]
+#     for i in range(n):
+#         temp=[]
+#         for j in range(n):
+#             print ("add->",i,j,":",end="")
+#             temp.append(int(input()))
+#         matrix.append(temp)
+#     return matrix
+b=matrix=[[0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0]]
+
+
+
+def callback():
     
+    for i in range(9):
+        for j in range(9):
+            t=b[i][j].get()
+            
+            if t!='' :
+                if 0<int(t)<10:
+                    matrix[i][j]=int(t)
+                else:
+                    exit
+                         
+            else:
+                matrix[i][j]=0
+            
+    if solver(matrix):
+         result_matrix(matrix)
+    else:
+        exit
+        
+
+      
+   
+def make_matrix(root,canvas):
+    for i in range(9):
+        for j in range(9):
+            b[i][j]= Entry(canvas, width=5)
+            b[i][j].grid(row=i, column=j)
+    button = tk.Button(root, text="get", width=10, command=callback)
+    button.pack()
+    
+    root.mainloop()
+
     
 def column(matrix, i):
     return [row[i] for row in matrix]  
 
-def sub_matrix(matrix,i,j):
+def sub_matrix(s_matrix,i,j):
     sm=[]
-    i=i-1 if i%2==1 else i
-    j=j-1 if j%2==1 else j
-    for row in range(2):
+    i=i-i%3
+    j=j-j%3
+    for row in range(3):
         temp=[]
-        for col in range(2):
-            temp.append(matrix[row+i][col+j])
+        for col in range(3):
+            temp.append(s_matrix[row+i][col+j])
         sm.append(temp)
     return (sm)
 
@@ -69,10 +104,10 @@ def check_col_matrix(col_matrix,num):
         return False
     
             
-def find_zero(matrix):
-    for i in range(4):
-        for j in range(4):
-            if matrix[i][j]==0:
+def find_zero(z_matrix):
+    for i in range(9):
+        for j in range(9):
+            if z_matrix[i][j]==0:
                 return (i,j)
             
     return None
@@ -86,27 +121,26 @@ def solver(matrix):
     else:    
         i,j=is_there
     
-    
-    for t in range(1,5):
+    for t in range(1,10):
         sm=sub_matrix(matrix,i,j)
         row_matrix=matrix[i]
-        col_matrix=column(matrix,j)                    
+        col_matrix=column(matrix,j)
+                        
         if check_row_matrix(row_matrix,t) and check_col_matrix(col_matrix,t) and check_sub_matrix(sm,t):
                 matrix[i][j]=t
                 if(solver(matrix)):      
                     return True
                 matrix[i][j]=0
-    
+
     return False
                     
-if __name__=="__main__": 
-    test_matrix=make_matrix(4) 
-    if solver(test_matrix):
-        print("solution is:")        
-        show_matrix(test_matrix)
-    else:
-        print("not a valid grid")
-
+if __name__=="__main__":
+    root=tk.Tk()
+    root.title("sudoku solver")
+    canvas=tk.Canvas(root,height=100,width=100)
+    canvas.pack()    
+    make_matrix(root,canvas) 
+    
 
 
 
