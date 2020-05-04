387d32c67a998685fedec2c66f50fe81274dda38
palak <p.thapar99@gmail.com>
2020-05-04 15:32:00 +0530

    final commit

--- sudoko.py
+++ sudoko.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import Entry
 from tkinter import messagebox
-#for GUI version
+#for result in GUI version
 def result_matrix(res_matrix):
     canvas2=tk.Canvas(root,height=100,width=100)
     canvas2.pack()
@@ -11,19 +11,6 @@ def result_matrix(res_matrix):
             b.grid(row=i, column=j)
             b.insert(0, res_matrix[i][j])
 
-        
-
-
-        
-# def make_matrix(n):
-#     matrix=[]
-#     for i in range(n):
-#         temp=[]
-#         for j in range(n):
-#             print ("add->",i,j,":",end="")
-#             temp.append(int(input()))
-#         matrix.append(temp)
-#     return matrix
 b=matrix=[[0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0],
@@ -34,45 +21,40 @@ b=matrix=[[0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0]]
 
-
-
-def callback():
-    
+#producing result on get button
+def callback():    
     for i in range(9):
         for j in range(9):
-            t=b[i][j].get()
-            
+            t=b[i][j].get()            
             if t!='' :
                 if 0<int(t)<10:
                     matrix[i][j]=int(t)
                 else:
-                    exit
-                         
+                    exit                         
             else:
-                matrix[i][j]=0
-            
+                matrix[i][j]=0            
     if solver(matrix):
          result_matrix(matrix)
     else:
         exit
         
-
-      
-   
+#creates the matrix   
 def make_matrix(root,canvas):
+   
     for i in range(9):
         for j in range(9):
             b[i][j]= Entry(canvas, width=5)
-            b[i][j].grid(row=i, column=j)
-    button = tk.Button(root, text="get", width=10, command=callback)
+            b[i][j].grid(row=i, column=j,ipady=5)
+    button = tk.Button(root, text="get result", width=10, bg='blue', fg='white', command=callback)
     button.pack()
     
     root.mainloop()
 
-    
+#get the column matrix    
 def column(matrix, i):
     return [row[i] for row in matrix]  
 
+#get the submatrix
 def sub_matrix(s_matrix,i,j):
     sm=[]
     i=i-i%3
@@ -84,43 +66,42 @@ def sub_matrix(s_matrix,i,j):
         sm.append(temp)
     return (sm)
 
+#checking if sub-matrix has that number
 def check_sub_matrix(sm,num):
     if not any(num in x for x in sm):
         return True
     else:
         return False
-    
+ 
+#checking if row matrix has that number    
 def check_row_matrix(row_matrix,num):
     if num not in row_matrix:
         return True
     else:
         return False
     
-
+#checking if column matrix has that number
 def check_col_matrix(col_matrix,num):
     if num not in col_matrix:
         return True
     else:
         return False
     
-            
+#finding empty spaces           
 def find_zero(z_matrix):
     for i in range(9):
         for j in range(9):
             if z_matrix[i][j]==0:
-                return (i,j)
-            
+                return (i,j)            
     return None
                    
-    
-def solver(matrix):
-        
+#actual logic solving function   
+def solver(matrix):        
     is_there=find_zero(matrix)
     if (not is_there):        
         return True
     else:    
         i,j=is_there
-    
     for t in range(1,10):
         sm=sub_matrix(matrix,i,j)
         row_matrix=matrix[i]
@@ -131,12 +112,17 @@ def solver(matrix):
                 if(solver(matrix)):      
                     return True
                 matrix[i][j]=0
-
     return False
+
+
+
+
                     
 if __name__=="__main__":
     root=tk.Tk()
     root.title("sudoku solver")
+    w = tk.Label(root, text="Your Sudoku Solver!")
+    w.pack()
     canvas=tk.Canvas(root,height=100,width=100)
     canvas.pack()    
     make_matrix(root,canvas) 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+if solver(b):
+    #     result_matrix(b)
+    # else:
+        
+    #     messagebox.showerror("error", "invalid values")
